1626d8f0f37468c91c3c4d77550a7ece52ffb4a5
surajsingh9389 <sk9389623488@gmail.com>
2026-02-13 22:48:22 +0530

    Complete ther phase 1

--- Phase1 JavaScript Core/Topic7.js
+++ Phase1 JavaScript Core/Topic7.js
@@ -92,3 +92,61 @@
 // }
 
 // check(5).then(res => console.log(res)).catch(err => console.log(err));
+
+
+// async function getData() {
+//    return "data";    
+// }
+
+// getData().then(console.log);
+
+// async function  handleUser() {
+//   try{
+//     const user = await fetchUser();
+//     const orders = await fetchOrders(user.id);
+//     const result = await processOrders(orders);
+//     console.log(result);
+//   }catch(err){
+//     console.error(err);
+//   }    
+// }
+
+// const [a, b] = await Promise.all([task1(), task2]);
+
+// Q1. 5
+// Q2. Handled
+// Q3. A, B, C
+// Q4. async function getUser(){
+//     const user = await fetch("/user")
+//     return user;
+// }
+
+// Q5. async function getData() {
+//     try {
+//     const data = await processData();
+//     const res = await saveData(data);
+//     console.log(res);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// async function demo() {
+//   console.log("A");
+//   await Promise.resolve("C").then(data => console.log(data));
+//   console.log("B");
+// }
+
+// demo();
+
+
+// Q1. promise
+// Q2. no 
+// Q3. it give us a promise which we can futher process using .then() or .catch()
+// Q4. first return imidiatly and the second one wait to finish.
+// Q5. no 
+// Q6. using Promise.all([])
+// Q7. it will throw a error which we can catch using .catch()
+// Q8. 0, 1, 3, 2
+// Q9. easy to understand and provide more readable code.
+// Q10. if use multiple await promises the all task run sequentioly but we can use Promise.all([]) to run all task parallel.
