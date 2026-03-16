0d6e1511b850c82c391865115eb280c7b6e00336
surajsingh9389 <sk9389623488@gmail.com>
2026-03-16 23:45:18 +0530

    production grade structure

---
+++ Phase 5 TypeScript/production_backend_structure.ts
@@ -0,0 +1,36 @@
+// src
+// │
+// ├ config
+// │   ├ db.ts
+// │   └ env.ts
+// │
+// ├ controllers
+// │   └ auth.controller.ts
+// │
+// ├ services
+// │   └ auth.service.ts
+// │
+// ├ repositories
+// │   └ user.repository.ts
+// │
+// ├ models
+// │   └ user.model.ts
+// │
+// ├ routes
+// │   └ auth.routes.ts
+// │
+// ├ middleware
+// │   ├ auth.middleware.ts
+// │   ├ errorHandler.ts
+// │   └ validate.middleware.ts
+// │
+// ├ utils
+// │   ├ AppError.ts
+// │   ├ asyncHandler.ts
+// │   └ generateToken.ts
+// │
+// ├ types
+// │   └ express.d.ts
+// │
+// ├ app.ts
+// └ server.ts
