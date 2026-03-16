1df5fec0c71740bde7e15a0a87ac47c4be5b79d3
surajsingh9389 <sk9389623488@gmail.com>
2026-03-16 22:50:29 +0530

    Request Logging + Security added

---
+++ Phase 5 TypeScript/Topic8.ts
@@ -0,0 +1,7 @@
+// 1. Helmet: A security package that automatically sets various HTTP response headers to protect your app from well-known web vulnerabilities like XSS (Cross-Site Scripting) and clickjacking.
+
+// 2. Morgan: An HTTP request logger that records details about incoming requests (like the URL, method, and response time) to your console or a file, which is helpful for debugging and monitoring traffic.
+
+// 3. Express-rate-limit: A middleware used to limit repeated requests from the same IP address, helping to prevent brute-force attacks and DoS (Denial of Service) abuse.
+
+// 4. CORS (Cross-Origin Resource Sharing): A package that configures your server to allow or restrict requests coming from different domains (origins), ensuring that only authorised websites can access your API.
--- Phase 5 TypeScript/practice/src/index.ts
+++ Phase 5 TypeScript/practice/src/index.ts
@@ -1,15 +1,34 @@
 import express from "express";
 import type { Request, Response } from "express";
+import helmet from "helmet"
+import morgan from "morgan"
+import cors from "cors"
+
 import authRoutes from "./routes/authRoutes.js"
 import "dotenv/config";
 import { connectDB } from "./config/database.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import { limiter } from "./utils/rateLimiter.js";
 
 
 
 const app = express();
 
+// parse incoming text-json 
 app.use(express.json());
+
+// Security Headers 
+app.use(helmet())
+
+// Allow your frontend to call API 
+app.use(cors())
+
+// Helps debugging API traffic 
+app.use(morgan("dev"))
+
+app.use(limiter)
+
+// prevent brute force login, API spam, bots
 app.use("/auth", authRoutes);
 
 connectDB();
