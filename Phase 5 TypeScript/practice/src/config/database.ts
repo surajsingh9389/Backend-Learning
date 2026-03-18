dad425db80ad21b27201588dfcfe4c98d182aa22
surajsingh9389 <sk9389623488@gmail.com>
2026-03-18 23:48:29 +0530

    mongodb config updated

--- Phase 5 TypeScript/practice/src/config/database.ts
+++ Phase 5 TypeScript/practice/src/config/database.ts
@@ -4,8 +4,8 @@ import { env } from "./env.js";
 
 export async function connectDB(){
     try {
-        await mongoose.connect(env.mongoUri);
-        console.log("MongoDB connected");
+        const conn = await mongoose.connect(env.mongoUri);
+        console.log(`Mongodb Connected ${conn.connection.host}`);
     } catch (err) {
         console.error("Database connection failed", err);
         process.exit(1);
